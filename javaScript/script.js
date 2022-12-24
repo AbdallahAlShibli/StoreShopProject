33b7121ea7dc8fae82cdc1998fb546a25ea0c0cb
AbdallahAlShibli <48906718+AbdallahAlShibli@users.noreply.github.com>
2022-12-24 23:28:26 +0400

    11:28

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,4 @@
-//dynamic slide show gallery
+//dynamic slide show gallery change image every 3 seconds
 let index = 0;
 showSlides();
 
@@ -16,12 +16,12 @@ function showSlides() {
     }
     slides0[index - 1].style.display = "block";
     dots0[index - 1].className += " active";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
 
 
-
+//dynamic slide show gallery change image by user click
 let indexSlide = index;
 
 showSlide(indexSlide);
@@ -55,7 +55,7 @@ function showSlide(n) {
 }
 
 
-
+//get current date and time
 var currentdate = new Date();
 var datetime = " Today is " + currentdate.getDate() + "/"
     + (currentdate.getMonth() + 1) + "/"
@@ -64,8 +64,47 @@ var datetime = " Today is " + currentdate.getDate() + "/"
     + currentdate.getMinutes() + ":"
     + currentdate.getSeconds();
 
-document.getElementById("welcomeP").innerHTML = "Welcome to the FFPF Website!" + datetime;
+document.getElementById("welcomeP").innerHTML = "Welcome to the FFPF Website!" + datetime;//add current date and time to welcome paragraph
 
 
 
 
+
+// Get the email input element
+const emailInput = document.getElementById('form5Example2');
+
+// Get the submit button element
+const submitButton = document.querySelector('button[type="submit"]');
+
+// Add a click event listener to the submit button
+submitButton.addEventListener('click', function (event) {
+    // Prevent the default action of the button (submitting the form)
+    event.preventDefault();
+
+    // Get the value of the email input
+    const email = emailInput.value;
+
+    // Validate the email
+    if (!isValidEmail(email)) {
+        // If the email is invalid, show an error message
+        alert('Please enter a valid email address');
+        return;
+    }
+
+    // If the email is valid, send the newsletter signup request
+    sendNewsletterSignupRequest(email);
+});
+
+function isValidEmail(email) {
+    // Use a regular expression to check if the email is in the correct format
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    return emailRegex.test(email);
+}
+
+function sendNewsletterSignupRequest(email) {
+    // Use an HTTP request to send the newsletter signup request to the server
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/newsletter-signup');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(`email=${encodeURIComponent(email)}`);
+}
